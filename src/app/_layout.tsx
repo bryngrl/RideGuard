b1839fce8d0510ac5c820ef3793be9aafabf454e
Author <geroliabryan024@gmail.com>
2026-08-21 16:45:13 +0800

    Importing the fonts and installing vector-icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
-
 
 SplashScreen.preventAutoHideAsync();
 
-// export default function TabLayout() {
-//   const colorScheme = useColorScheme();
-//   return (
-//     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-//       <AnimatedSplashOverlay />
-//       <AppTabs />
-//     </ThemeProvider>
-//   );
-// }
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
 
-// TODO: Make this like a route handler nalang
+  const [fontsLoaded, fontError] = useFonts({
+    'EloquiaDisplay-ExtraBold': require('../../assets/fonts/Typekiln - EloquiaDisplay-ExtraBold.otf'),
+    'EloquiaText-ExtraLight': require('../../assets/fonts/Typekiln - EloquiaText-ExtraLight.otf'),
+    'Geist-Regular': require('../../assets/fonts/Geist-Regular.ttf'),
+    'Geist-Medium': require('../../assets/fonts/Geist-Medium.ttf'),
+    'Geist-SemiBold': require('../../assets/fonts/Geist-SemiBold.ttf'),
+    'Geist-Thin': require('../../assets/fonts/Geist-Thin.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
+}
+
