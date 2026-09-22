263936b4d1af26e233dfd4bd3a50da52b40b0d6c
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-23 02:27:36 +0800

    feat: add token request type for ably

--- src/features/auth/auth.types.ts
+++ src/features/auth/auth.types.ts
@@ -1,3 +1,13 @@
 export type IsOldUserResponse = {
   isOldUser: boolean;
 };
+
+export type TokenRequest = {
+  capability: string;
+  clientId?: string;
+  keyName: string;
+  mac: string;
+  nonce: string;
+  timestamp: number;
+  ttl?: number;
+};
