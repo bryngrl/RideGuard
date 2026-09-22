580f0f6ca67e8330f4bcab07b1bea65fe1148879
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-23 02:27:53 +0800

    feat: add get ably token

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "@/lib/api/client";
 import type { ApiResponse } from "@/lib/api/types";
-import { IsOldUserResponse } from "./auth.types";
+import { IsOldUserResponse, TokenRequest } from "./auth.types";
 
 export async function checkIsOldUser(): Promise<boolean> {
   return apiClient
@@ -12,10 +12,10 @@ export async function checkIsOldUser(): Promise<boolean> {
     });
 }
 
-export async function getAblyToken(): Promise<string> {
+export async function getAblyToken(): Promise<TokenRequest> {
   return apiClient
-    .get<ApiResponse<{ token: string }>>("/auth/ably-token")
-    .then((response) => response.data.data.token)
+    .get<ApiResponse<TokenRequest>>("/auth/ably-token")
+    .then((response) => response.data.data)
     .catch((error) => {
       console.error("Error fetching Ably token:", error);
       throw new Error("Failed to fetch Ably token");
